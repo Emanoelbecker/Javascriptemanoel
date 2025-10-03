24ec42057e8b84ae8e59b19e2f44bef9551a0818
Emanoel Becker <emanoelbc01@gmail.com>
2025-10-03 16:36:36 -0300

    Versão tratando rotas

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -9,8 +9,8 @@ console.log(`Soma: ${soma(5, 3)}`);
 
 console.log(`Subtração: ${subtrai(10, 4)}`);
 
-console.log(`Multiplicação: ${multiplica(5, 5)}`);
+console.log(`Multiplicação: ${multiplica(6, 7)}`);
 
 console.log(`Divisão: ${divide(169, 13)}`);
 
-console.log(calculadora.idade("joão", 2000, 2050));
+console.log(calculadora.idade("joão", 1, 2025));
