729faabd5fe2237e8fe8a6aab5274174718b31d2
Emanoel Becker <emanoelbc01@gmail.com>
2025-10-24 16:54:11 -0300

    rotaloginNãoFuncionando

--- nodejs/saudacao-mvc/app.js
+++ nodejs/saudacao-mvc/app.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
 
+const produtosRoutes = require ('./routes/produtos')
+app.use('/produtos', produtosRoutes )
+
+const usuariosRoutes = require ('./routes/usuarios')
+app.use('/usuarios', usuariosRoutes )
+
 // Servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
